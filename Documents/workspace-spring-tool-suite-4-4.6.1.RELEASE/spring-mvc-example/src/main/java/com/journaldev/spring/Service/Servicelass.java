44d0f6e29bcfd9db3d2da8ed3ea6b97d1b7bd40b
ajitheee22 <ajitheee22@gmail.com>
2020-05-02 13:52:52 +0530

    first commit

---
+++ Documents/workspace-spring-tool-suite-4-4.6.1.RELEASE/spring-mvc-example/src/main/java/com/journaldev/spring/Service/Servicelass.java
@@ -0,0 +1,14 @@
+package com.journaldev.spring.Service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Servicelass {
+
+	
+	public String getname(String name) {
+		
+		return "Weclome to Spring"+name;
+		
+	}
+}
---
+++ Documents/workspace-spring-tool-suite-4-4.6.1.RELEASE/spring-mvc-example/src/main/java/com/journaldev/spring/controller/Ajithconfig.java
@@ -0,0 +1,26 @@
+package com.journaldev.spring.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.journaldev.spring"})
+@EnableWebMvc
+public class Ajithconfig extends WebMvcConfigurerAdapter
+{
+
+	@Bean
+	public InternalResourceViewResolver viewresolver() {
+	
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/views/");
+		vr.setSuffix(".jsp");
+		
+		return vr;
+	}
+}
